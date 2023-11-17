8e599b05e66d956aa7f56936a83fcf9e5098bf50
seaona <mariona@gmx.es>
2023-11-17 09:05:27 +0100

    module 5 - pokedex (finished)

--- 5-pokedex-project/src/Pokecard.js
+++ 5-pokedex-project/src/Pokecard.js
@@ -8,11 +8,13 @@ class Pokecard extends Component {
     
     render() {
         let imgSrc = `${baseImgUrl}${this.props.id}.png`
-        {console.log("PROOOP")}
+        
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{this.props.name}</h1>
-                <img className="Pokecard-img" src={imgSrc} alt={this.props.name}/>
+                <div className="Pokecard-image">
+                    <img src={imgSrc} alt={this.props.name}/>
+                </div>
                 <div className="Pokecard-data">Type: {this.props.type}</div>
                 <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>

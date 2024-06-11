16fc045951b9bf407c05da6b2878e8745d1a9383
seaona <mariona@gmx.es>
2024-06-11 09:13:04 +0200

    module 24 - massive color project - router

--- 24-massive-color-project/src/App.js
+++ 24-massive-color-project/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Routes, useParams } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+
+function PaletteWrapper() {
+  const { id } = useParams();
+  const palette = seedColors.find(palette => palette.id === id);
+  if (!palette) {
+    return <h1>Palette not found</h1>;
+  }
+
+  const generatedPalette = generatePalette(palette);
+  return <Palette palette={generatedPalette} />;
+
+}
 
 class App extends Component {
   render() {
     return (
-        <Palette palette={generatePalette(seedColors[4])}/>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<PaletteList palettes={seedColors}/>}
+          />
+          <Route 
+            path="/palette/:id"
+            element={<PaletteWrapper/>}
+          />
+        </Routes>
+      </div>
     );
   }
 
